30e5d3fae1b2616c3fdf2fb97dd4ec1a153ca775
Rakesh443 <69915552+Rakesh443@users.noreply.github.com>
2021-01-21 22:25:23 +0530

    Delete django_demo directory

--- django_demo/homepage/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import HomePageOffersCarousel
-
-admin.site.register(HomePageOffersCarousel)
-# Register your models here.
--- django_demo/homepage/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class HomePageOffersCarousel(models.Model):
-    itemName = models.CharField(max_length=100)
-    itemDescription= models.TextField()
-    itemOffer = models.CharField(max_length=100)
-    itemImage = models.ImageField(upload_to='pics') 
