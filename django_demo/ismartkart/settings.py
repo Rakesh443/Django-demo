e9d4a342df0a447578910c4d641657fd1424a862
Rakesh443 <rk20035@gmail.com>
2021-01-21 21:52:30 +0530

    ready for deployement

--- django_demo/ismartkart/settings.py
+++ django_demo/ismartkart/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$lby+q@smv$gn^x%aiyauod2o0$t^md7x#*78d@p3&kkt)4&1r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
